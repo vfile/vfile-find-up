3cdb4807b07f318ddb8a70d5dc19e0bf3dd18d7a
Titus Wormer <tituswormer@gmail.com>
2023-02-08 17:48:02 +0100

    Add exports of `Callback`, `CallbackOne` type

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /**
  * @typedef {import('./lib/index.js').Assert} Assert
+ * @typedef {import('./lib/index.js').Callback} Callback
+ * @typedef {import('./lib/index.js').CallbackOne} CallbackOne
  * @typedef {import('./lib/index.js').Test} Test
  */
 
